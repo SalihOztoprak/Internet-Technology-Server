5409d5fc6f6dac4fad51977493f0112dc50b35a4
Salih Oztoprak <salih.58@live.nl>
2018-11-23 13:30:58 +0100

    Begin phase project

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,6 +9,14 @@ public class Main {
     }
 
     public void run(){
+        connectToServer();
+    }
 
+    public static void connectToServer() {
+        try (ServerSocket serverSocket = new ServerSocket(1337)) {
+            Socket connectionSocket = serverSocket.accept();
+        } catch (IOException ioe) {
+            System.out.println(ioe.getStackTrace());
+        }
     }
 }
