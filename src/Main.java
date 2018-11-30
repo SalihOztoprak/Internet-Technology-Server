bd7a15d331615efccbb471d4e9f2da6027655064
Salih Oztoprak <salih.58@live.nl>
2018-11-30 13:05:02 +0100

    Merge branch 'master' of https://github.com/SalihOztoprak/Internet-technologie
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import sun.security.provider.MD5;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -16,30 +18,20 @@ public class Main {
 
     public static void connectToServer() {
         //Try connect to the server on an unused port eg 9991. A successful connection will return a socket
-        try(ServerSocket serverSocket = new ServerSocket(1337)) {
+        try (ServerSocket serverSocket = new ServerSocket(1337)) {
             Socket connectionSocket = serverSocket.accept();
-
+            
             //Create Input&Outputstreams for the connection
             InputStream inputToServer = connectionSocket.getInputStream();
             OutputStream outputFromServer = connectionSocket.getOutputStream();
 
-            Scanner scanner = new Scanner(inputToServer, "UTF-8");
             PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
 
             serverPrintOut.println("HELO Hello World! Enter Peace to exit.");
 
             //Have the server take input from the client and echo it back
             //This should be placed in a loop that listens for a terminator text e.g. bye
-            boolean done = false;
-
-            while(!done && scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                serverPrintOut.println("HELO Echo from <Your Name Here> Server: " + line);
-
-                if(line.toLowerCase().trim().equals("peace")) {
-                    done = true;
-                }
-            }
+            serverPrintOut.println("HELO Echo from <Your Name Here> Server: " + inputToServer);
         } catch (IOException e) {
             e.printStackTrace();
         }
