605061d7f446b79bb79744a67fe06105231702bc
thomastunc <thomastunc@hotmail.com>
2018-11-30 14:03:10 +0100

    Added new things and stuff

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Base64;
-import java.util.Scanner;
 
 public class Main {
     private final static int PORT = 1337;
+    private static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
 
     public static void main(String[] args) {
         new Main().run();
@@ -23,21 +24,50 @@ public class Main {
                 InputStream is = socket.getInputStream();
                 OutputStream os = socket.getOutputStream();
 
-                // Block thread until socket input has been read.
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-                PrintWriter writer = new PrintWriter(new OutputStreamWriter(os, "UTF-8"), true);
+                sendMessage(os, "HELO");
+                String message = readMessage(is);
+                sendMessage(os, "+OK " + encodeMessage(message));
 
-                writer.println("HELO");
-                writer.flush();
+                String username = null;
+                if (message != null) {
+                    username = message.replace("HELO ", "");
+                }
 
-                String line = reader.readLine();
-                final String encodedMessage = Base64.getEncoder().encodeToString(MD5.getMd5(line));
-                writer.println("+OK " + encodedMessage);
-                ClientHandler clientHandler = new ClientHandler(is,os,socket);
+                ClientHandler clientHandler = new ClientHandler(username, socket);
+                clientHandlers.add(clientHandler);
+                clientHandler.start();
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void broadcastMessage(ClientHandler handler, String message) {
+
+    }
+
+    public static void sendMessage(OutputStream outputStream, String message) {
+        try {
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream, "UTF-8"), true);
+            writer.println(message);
+            writer.flush();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String readMessage(InputStream inputStream) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            return reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String encodeMessage(String message){
+        return Base64.getEncoder().encodeToString(MD5.getMd5(message));
     }
 }
